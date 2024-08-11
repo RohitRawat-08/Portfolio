b3792cdaf0483ea3df7783908029f3b6df5daec4
Rohit <rsrawat2929@gmail.com>
2024-08-12 01:34:22 +0530

    update

---
+++ script.js
@@ -0,0 +1,52 @@
+
+let lhome = document.getElementById("lhome");
+let labout = document.getElementById("labout");
+let lskills = document.getElementById("lskills");
+let lprojects = document.getElementById("lprojects");
+let lcontact = document.getElementById("lcontact");
+    
+lhome.addEventListener("click",()=>{
+    lhome.style.color="aqua";
+
+    labout.style.removeProperty('color');
+    lskills.style.removeProperty('color');
+    lprojects.style.removeProperty('color');
+    lcontact.style.removeProperty('color');
+
+});
+
+labout.addEventListener("click",()=>{
+    labout.style.color="aqua";
+    
+    lhome.style.removeProperty('color');
+    lskills.style.removeProperty('color');
+    lprojects.style.removeProperty('color');
+    lcontact.style.removeProperty('color');
+});
+
+lskills.addEventListener("click",()=>{
+    lskills.style.color="aqua";
+
+    lhome.style.removeProperty('color');
+    labout.style.removeProperty('color');
+    lprojects.style.removeProperty('color');
+    lcontact.style.removeProperty('color');
+});
+
+lprojects.addEventListener("click",()=>{
+    lprojects.style.color="aqua";
+
+    lhome.style.removeProperty('color');
+    labout.style.removeProperty('color');
+    lskills.style.removeProperty('color');
+    lcontact.style.removeProperty('color');
+});
+
+lcontact.addEventListener("click",()=>{
+    lcontact.style.color="aqua";
+
+    lhome.style.removeProperty('color');
+    labout.style.removeProperty('color');
+    lskills.style.removeProperty('color');
+    lprojects.style.removeProperty('color');
+});
